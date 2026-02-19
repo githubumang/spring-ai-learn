3ab7e0e5812ec6445afdb7a4db82867f2903cda8
UMANG AGRAWAL <umangagrawal@UMANGs-MacBook-Air.local>
2026-02-19 14:38:20 +0530

    Add prompts

--- src/main/java/com/springcode/spring_ai/controller/HelloController.java
+++ src/main/java/com/springcode/spring_ai/controller/HelloController.java
@@ -1,6 +1,12 @@
 package com.springcode.spring_ai.controller;
 
+import java.util.Map;
+
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     private ChatClient chatClient;
+
+    @Value("classpath:prompts/celebDetailPrompt.st")
+    private Resource celebDetailPrompt;
 
     public HelloController (ChatClient.Builder builder) {
         chatClient = builder.build();
@@ -22,6 +31,29 @@ public class HelloController {
                     .getResult()
                     .getOutput()
                     .getText();
+    }
+
+    @GetMapping("/celeb")
+    public String getCelebDetails(@RequestParam String name ) {
+        String message ="""
+                List the details of the Famous personality {name} along with their Carrier achievements.
+                Please return the answer in html format.
+                """;
+
+        PromptTemplate template = new PromptTemplate(celebDetailPrompt);
+        
+        Prompt prompt = template.create(
+        Map.of("name", name)
+        ) ;
+        // System.out.println(prompt);
+        // return name;
+        return chatClient
+                .prompt(prompt)
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput()
+                .getText();
     }
     
 }
